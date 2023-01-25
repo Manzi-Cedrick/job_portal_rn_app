e50dfb65f0aaee836af944ee2e6a24ef6e78230b
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-01-25 13:15:19 +0200

    [add] Screen navigator

---
+++ src/routes/MainRoute.js
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createStackNavigator } from '@react-navigation/stack';
+import Welcome from '../screens/Welcome';
+import Login from '../screens/Login';
+const MainRoute = () => {
+    const Stack = createStackNavigator();
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="welcome" component={Welcome} options={{
+        headerShown: false
+      }}/>
+      <Stack.Screen name="Login" component={Login} options={{
+        headerShown: false
+      }}/>
+    </Stack.Navigator>
+  )
+}
+
+export default MainRoute
