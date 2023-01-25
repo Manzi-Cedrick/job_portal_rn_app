fb2485c6cb0e950e5e6a9291710d7bbe1223b5cf
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-01-25 13:14:19 +0200

    [add] Logo

---
+++ src/components/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Path, Svg } from 'react-native-svg'
+
+const Logo = () => {
+  return (
+    <Svg width="23" height="26" viewBox="0 0 23 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+<Path d="M13.9612 14.1161C13.9612 11.6201 15.9846 9.59668 18.4806 9.59668C20.9766 9.59668 23 11.6201 23 14.1161V15.2337C23 17.7297 20.9766 19.7532 18.4806 19.7532C15.9846 19.7532 13.9612 17.7297 13.9612 15.2337V14.1161Z" fill="#4CA6A8"/>
+<Path fill-rule="evenodd" clip-rule="evenodd" d="M10.457 14.8243V0.410738H11.8817V0H0V0.410738H1.41818V16.1775C1.36304 20.9655 4.48876 29.2871 17.4334 24.2703C13.1798 24.2703 10.457 22.6147 10.457 14.8243Z" fill="#4CA6A8"/>
+</Svg>
+
+  )
+}
+
+export default Logo
