b3f2f72c09fa151ef12cf8faeec765701b64313a
Manzi Cedrick <cedrickmanzii0@gmail.com>
2023-02-27 16:04:55 +0200

    [add] icons addition

---
+++ src/assets/MenuElipsis.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Path, Svg } from 'react-native-svg'
+
+const MenuElipsis = () => {
+  return (
+    <Svg width="18" height="14" viewBox="0 0 18 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <Path d="M1 13H8M1 1H17H1ZM1 7H17H1Z" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+    </Svg>    
+  )
+}
+
+export default MenuElipsis
